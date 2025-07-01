2099ed14332493680ebe13bd83f4a7abbae9c1d4
Nduoma132 <nduoma132@gmail.com>
2025-07-01 12:54:31 +0100

    My first commit

---
+++ src/request_response.py
@@ -0,0 +1,19 @@
+# This file defines the request and response models for the STUPID-LM API.
+
+from pydantic import BaseModel
+from typing import Optional, Union
+
+class WordRequest(BaseModel):
+    input: str
+    word_count: Optional[int]
+
+class SuccessResponse(BaseModel):
+    response_code: int
+    response_message: str
+    output: Optional[str]
+
+class ErrorResponse(BaseModel):
+    response_code: int
+    response_message: str
+
+response = Union[SuccessResponse, ErrorResponse]
